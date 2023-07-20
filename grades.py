494cd34a772408b7501bbfc9cc2da48e8c44b384
Marcel Suter <marcel.suter@bzz.ch>
2023-07-20 09:57:52 +0200

    added solution

--- grades.py
+++ grades.py
@@ -1,6 +1,21 @@
 def main():
-    # Write your program here and remove the line "pass"
-    pass
+    points = int(input('Give points [0-100]:\n'))
+    if points < 0:
+        print('impossible!')
+    elif points <= 49:
+        print(1)
+    elif points <= 59:
+        print(2)
+    elif points <= 69:
+        print(3)
+    elif points <= 79:
+        print(4)
+    elif points <= 89:
+        print(5)
+    elif points <= 100:
+        print(6)
+    elif points > 100:
+        print('incredible!')
 
 
 if __name__ == '__main__':
