d54d01617d1adf762cb29334c0e5d6b6298d4b50
Marcel Suter <marcel.suter@bzz.ch>
2023-07-20 09:56:21 +0200

    initial commit

---
+++ grades_test.py
@@ -0,0 +1,33 @@
+from grades import main
+
+
+def test_impossible(capsys, monkeypatch):
+    inputs = iter([-1])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'impossible!\n'
+
+
+def test_failed(capsys, monkeypatch):
+    inputs = iter([49])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'failed\n'
+
+
+def test_grade4(capsys, monkeypatch):
+    inputs = iter([80])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == '4\n'
+
+
+def test_incredible(capsys, monkeypatch):
+    inputs = iter([102])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'incredible!\n'
